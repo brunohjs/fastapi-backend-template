14eb09f510dd2f76a78b8eae3b6f66bbeab68c86
bruno <brunohjs@gmail.com>
2025-01-10 01:10:29 -0300

    Inclusão de mais pacotes de desenvolvimento

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -1,24 +1,25 @@
-import decouple
+import os
+
 import pydantic_settings
 
 
 class BaseSettings(pydantic_settings.BaseSettings):
-    APP_NAME: str = decouple.config("APP_NAME", "Minha API FastAPI")
-    API_VERSION: str = decouple.config("API_VERSION", "v1")
-    DEBUG: bool = decouple.config("DEBUG", True)
-    DESCRIPTION: str = decouple.config("DESCRIPTION", "")
-    ENVIRONMENT: str = decouple.config("ENVIRONMENT", "DEV")
+    APP_NAME: str = os.environ.get("APP_NAME", "Minha API FastAPI")
+    API_VERSION: str = os.environ.get("API_VERSION", "v1")
+    DEBUG: bool = os.environ.get("DEBUG", True)
+    DESCRIPTION: str = os.environ.get("DESCRIPTION", "")
+    ENVIRONMENT: str = os.environ.get("ENVIRONMENT", "DEV")
 
-    API_HOST: str = decouple.config("API_HOST", "localhost")
-    API_PORT: int = decouple.config("API_PORT", "8000")
-    API_PREFIX: str = decouple.config("API_PREFIX", "")
-    DOCS_URL: str = decouple.config("DOCS_URL", "/docs")
-    REDOC_URL: str = decouple.config("REDOC_URL", "/redoc")
+    API_HOST: str = os.environ.get("API_HOST", "localhost")
+    API_PORT: int = os.environ.get("API_PORT", "8000")
+    API_PREFIX: str = os.environ.get("API_PREFIX", "")
+    DOCS_URL: str = os.environ.get("DOCS_URL", "/docs")
+    REDOC_URL: str = os.environ.get("REDOC_URL", "/redoc")
 
-    DATABASE_URL: str = decouple.config("DATABASE_URL", "sqlite:///./test.db")
-    DATABASE_USERNAME: str = decouple.config("DATABASE_USERNAME", "")
-    DATABASE_PASSWORD: str = decouple.config("DATABASE_PASSWORD", "")
-    DATABASE_PORT: int = decouple.config("DATABASE_PORT", 8191)
+    DATABASE_URL: str = os.environ.get("DATABASE_URL", "sqlite:///./test.db")
+    DATABASE_USERNAME: str = os.environ.get("DATABASE_USERNAME", "")
+    DATABASE_PASSWORD: str = os.environ.get("DATABASE_PASSWORD", "")
+    DATABASE_PORT: int = os.environ.get("DATABASE_PORT", 8191)
 
     class Config:
         env_file = ".env"  # Define que as variáveis podem ser carregadas de um arquivo .env
